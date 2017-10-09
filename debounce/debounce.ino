bd06cecce4bb3291b9cd5de0c7c9370daa3a72e0
ethan <ethan@172.27.72.151>
2017-10-09 13:24:52 -0500

    Finished

---
+++ debounce/debounce.ino
@@ -0,0 +1,21 @@
+unsigned long lastTime = millis();
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(2, INPUT);
+  attachInterrupt(0, pin_ISR, RISING);
+  digitalWrite(2, HIGH);
+}
+
+void loop(){
+  
+}
+
+void pin_ISR(){
+  unsigned long tiempo = millis();
+  if(tiempo - lastTime >= 100){
+    Serial.println("Triggered");
+    lastTime = tiempo;
+  }
+}
+
